5e5b1ab58420b8f4b7b9e2de974636db14300a3b
SakinaMolvi-Code <sakinamolvi1@gmail.com>
2025-01-27 11:09:41 +0530

    header of all the pages

---
+++ php/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style><?php include 'CSS/landingPage.css'; ?></style>
+    <title>Landing Page</title>
+</head>
+
+    <body class="p-0 m-0 border-box flex-col relative">
+  <div class="bg-fuchsia-600">
+    <nav class="list-none overflow-hidden">
+      <ul class="flex flex-wrap w-full block py-4 px-3 text-black ">
+        <li class="text-center px-3"><a class="hover:underline" href="#">Home</a></li>
+        <li class="text-center px-3"><a class="hover:underline" href="#">About</a></li>
+        <li class="text-center px-3"><a class="hover:underline" href="#">Contact</a></li>
+      </ul>
+    </nav>
+  </div>
+
+  <!---Tailwind Css-->
+  <script src="https://cdn.tailwindcss.com"></script>
+
+
+  </body>
+  </html>
